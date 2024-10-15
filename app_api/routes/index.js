aff39ad17b7f7eaa3074a07df0f5b2ff3cd4dacf
bgjschool2020 <bgjeducation@outlook.com>
2024-10-15 14:19:07 -0600

    haylee is about to try

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,13 +1,16 @@
-const express = require("express"); // Express app
+const express = require('express'); // Express app
 const router = express.Router(); // Router logic
 
 // This is where we import the controllers we will route
-const tripsController = require("../controllers/trips");
+const tripsController = require('../controllers/trips');
 
 // Define route for our trips endpoint
-router.route("/trips").get(tripsController.tripsList); // GET Method routes tripList
+router
+    .route('/trips')
+    .get(tripsController.tripsList) // GET Method routes tripList
+    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
 
 // GET method routes tripsFindByCode - requires parameter
-router.route("/trips/:tripCode").get(tripsController.tripsFindByCode);
+router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
 
 module.exports = router;
