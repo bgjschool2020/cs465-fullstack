507abd6d28732caafce4ee360e499e66a291587b
bgjschool2020 <bgjeducation@outlook.com>
2024-10-17 12:32:22 -0600

    Module 7 completed baseline

--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-card',
@@ -10,16 +11,23 @@ import { Trip } from '../models/trip';
   templateUrl: './trip-card.component.html',
   styleUrl: './trip-card.component.css',
 })
-export class TripCardComponent {
+export class TripCardComponent implements OnInit {
   @Input('trip') trip: any;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
 
   ngOnInit(): void {}
+
+  public isLoggedIn() {
+    return this.authenticationService.isLoggedIn();
+  }
 
   public editTrip(trip: Trip) {
     localStorage.removeItem('tripCode');
     localStorage.setItem('tripCode', trip.code);
-    this.router.navigate(['/edit-trip']);
+    this.router.navigate(['edit-trip']);
   }
 }
